8478266a85326cc57426e59c3179beda7dc0a6f9
Kolpak <164049849+Horven228@users.noreply.github.com>
2025-10-10 21:59:30 +0300

    Delete yatube_api directory

--- yatube_api/posts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Group, Comment, Follow
-
-admin.site.register(Post)
-admin.site.register(Group)
-admin.site.register(Comment)
-admin.site.register(Follow)
